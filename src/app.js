cb8ca35a26e667ea3b4f9ba281ef1848af4c300d
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2024-01-02 12:21:10 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 app.use(express.json());
 
 // Write a GET route to return the value stored in the NUMBER variable stored in the .env file
@@ -10,6 +14,14 @@ app.use(express.json());
 */
 app.get('/api/get-env', (req, res) => {
    //Write your code here
+   const secretNumber= process.env.NUMBER;
+
+   if(secretNumber){
+      res.status(200).json({number: secretNumber});
+   }
+   else{
+     res.status(404).json({error:'Not Found'});
+   }
 });
 
 module.exports = app;
